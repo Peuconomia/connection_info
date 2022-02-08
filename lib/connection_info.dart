c05213568b96154aeba764969bc97edd4cf88eb3
Suman Adhikari <expertxkid@gmail.com>
2022-02-08 23:26:29 +0545

    docs: document connection_info.dart and connection_state.dart

--- lib/connection_info.dart
+++ lib/connection_info.dart
@@ -7,18 +7,37 @@ import 'package:flutter/foundation.dart';
 
 import 'models/connection_state.dart';
 
+/// [ConnectionController] class is a controller class that provides
+/// both a stream named [stream] and a future named [isConnected] for checking
+/// the current state of the internet conenction
 class ConnectionController {
+  /// [serverUrl] represents the name of the server e.g. google.com.
   String serverUrl;
+
+  /// [repeatInterval] represents the interval to check for connection
+  /// periodically.
   Duration repeatInterval;
+
+  /// [showErrorInDebugMode] flag is used for to show error message in debug mode
+  /// or not.
   final bool showErrorInDebugMode;
+
+  /// [_timer] is a timer that runs periodically for checking connection.
   Timer? _timer;
 
+  /// [_controller] is the [StreamController] used for the stream.
   late final StreamController _controller;
 
+  /// [stream] is the getter for the [_controller]'s [stream] instance.
   Stream get stream => _controller.stream;
 
+  /// [_previousState] holds the previous connection state.
+  ///
+  /// We only update the connection information to the user only if current state
+  /// does not match the previous state.
   ConnectionState? _previousState;
 
+  /// This is the constructor.
   ConnectionController({
     this.serverUrl = 'google.com',
     this.repeatInterval = const Duration(
@@ -53,6 +72,9 @@ class ConnectionController {
     );
   }
 
+  /// [isConnected] is the future that returns a [ConnectionState] object.
+  ///
+  /// It can be either true or false.
   Future<ConnectionState> isConnected() async {
     try {
       final lookupResult = await InternetAddress.lookup(serverUrl);
@@ -72,10 +94,15 @@ class ConnectionController {
     return _previousState!;
   }
 
+  /// [_setCurrentState] sets the new state.
+  ///
+  /// It sets [_previousState] only if [_previousState] is different from
+  /// [newState] or if [_previousState] is null. Furthermore, it pushes to the
+  /// stream as well if [_controller.hasListener] is true and [_controller.isClosed]
+  /// is false and [_controller.isPaused] is false.
   void _setCurrentState(ConnectionState newState) {
     if (_previousState == null || newState != _previousState) {
       _previousState = newState;
-      print(_previousState);
       if (_controller.hasListener &&
           !_controller.isClosed &&
           !_controller.isPaused) {
--- lib/models/connection_state.dart
+++ lib/models/connection_state.dart
@@ -4,6 +4,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'connection_state.freezed.dart';
 
+/// A model class to determine the current state of the internet connectivity
+///
+/// [isConnected] gives the current status of internet connectivity.
 @freezed
 class ConnectionState with _$ConnectionState {
   factory ConnectionState({
