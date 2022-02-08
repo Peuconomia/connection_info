5470c49a8c42d2b22eadefcd07b6bad720f8f347
Suman Adhikari <expertxkid@gmail.com>
2022-02-08 17:22:24 +0545

    feat: add initial test case where wrong url is sent for connection testing

--- test/connection_info_test.dart
+++ test/connection_info_test.dart
@@ -1,12 +1,20 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:connection_info/connection_info.dart';
+import 'package:connection_info/connection.dart';
+import 'package:connection_info/models/connection_state.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('Connection Controller tests', () {
+    test('send invalid server url and get error', () async {
+      final controller = ConnectionController(
+        serverUrl: 'http://asdom',
+        repeatInterval: const Duration(seconds: 1),
+      );
+
+      expect(
+          controller.stream,
+          emitsInOrder(
+            [ConnectionState(isConnected: false)],
+          ));
+    }, timeout: const Timeout(Duration(seconds: 2)));
   });
 }
